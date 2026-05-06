66dc79f21cad4dfd5c83f0da875f5c0cd8da9030
jocateles13032009-collab <jocateles13032009@gmail.com>
2026-05-06 14:35:52 -0300

    Add files via upload

---
+++ acai/js/dados.js
@@ -0,0 +1,8 @@
+// Dados do cardápio/serviços do cliente.
+// Edite este arquivo para alterar os itens exibidos no site.
+const ITENS = [
+  { nome: "Açaí 300ml", descricao: "Descrição breve do item.", preco: "R$ 10,00" },
+  { nome: "Açaí 500ml", descricao: "Descrição breve do item.", preco: "R$ 15,00" },
+  { nome: "Tigelão 1L", descricao: "Descrição breve do item.", preco: "R$ 20,00" },
+  { nome: "Smoothie Banana", descricao: "Descrição breve do item.", preco: "R$ 25,00" }
+];
