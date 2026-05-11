d01c60f50258abac33b26dab34a4bdf469ab4a20
jocateles13032009-collab <jocateles13032009@gmail.com>
2026-05-11 16:01:34 -0300

    Add files via upload

---
+++ acai/Js/dados.js
@@ -0,0 +1,65 @@
+ITENS = [
+  {
+    id: 1,
+    nome: "Açaí 300ml",
+    descricao: "2 acompanhamentos inclusos",
+    preco: 10,
+    imagem: ""
+  },
+
+  {
+    id: 2,
+    nome: "Açaí 500ml",
+    descricao: "4 acompanhamentos inclusos",
+    preco: 15,
+    imagem: "500 ml.png"
+  },
+
+  {
+    id: 3,
+    nome: "Açaí 700ml",
+    descricao: "6 acompanhamentos inclusos",
+    preco: 20,
+    imagem: "https://images.unsplash.com/photo-1625944230945-1b7dd3bdbb6b"
+  },
+
+  {
+    id: 4,
+    nome: "Açaí Especial",
+    descricao: "Nutella + Morango + Leite condensado",
+    preco: 25,
+    imagem: "https://images.unsplash.com/photo-1604908177522-429d3b8d6c7d"
+  },
+
+  {
+    id: 5,
+    nome: "Milkshake de Açaí",
+    descricao: "Milkshake cremoso de açaí",
+    preco: 18,
+    imagem: "https://images.unsplash.com/photo-1579954115545-a95591f28bfc"
+  },
+
+  {
+    id: 6,
+    nome: "Barca de Açaí",
+    descricao: "Açaí gigante para compartilhar",
+    preco: 45,
+    imagem: "https://images.unsplash.com/photo-1504674900247-0877df9cc836"
+  },
+
+  {
+    id: 7,
+    nome: "Vitamina Tropical",
+    descricao: "Banana, morango e açaí",
+    preco: 14,
+    imagem: "https://images.unsplash.com/photo-1623065422902-30a2d299bbe4"
+  },
+
+  {
+    id: 8,
+    nome: "Combo Casal",
+    descricao: "2 açaís 500ml + toppings",
+    preco: 35,
+    imagem: "https://images.unsplash.com/photo-1515003197210-e0cd71810b5f"
+  }
+];
